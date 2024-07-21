73bb0eba58cac5504e8781a6881e5d726da6c726
minsun309 <pminsun309@gmail.com>
2024-07-22 01:12:53 +0900

    Test: 깃허브 액션 배포 테스트7 - build.sh repo명수정

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./[TtorangttorangWeb]/* ./output
-cp -R ./output ./[TtorangttorangWeb]/
+cp -R ./TtorangttorangWeb/* ./output
+cp -R ./output ./TtorangttorangWeb/
