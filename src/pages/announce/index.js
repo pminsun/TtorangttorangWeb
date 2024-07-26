da84d02efeee73c7c3879b5d4d3855c2532b7c61
pminsun <pminsun309@gmail.com>
2024-07-26 16:45:21 +0000

    Refactor: zustand 발표문 관련 상태관리

--- src/pages/announce/index.js
+++ src/pages/announce/index.js
@@ -7,6 +7,7 @@ import HighlightWithinTextarea from 'react-highlight-within-textarea';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Image from 'next/image';
 import * as LocalImages from '@/utils/imageImports';
+import useStore from '../store/store';
 
 const testTxt =
   "안녕하세요, 여러분. 오늘은 디자인 프로세스에서 중요한 역할을 하는 퍼소나(Persona)에 대해 이야기해보겠습니다. 퍼소나는 디자인 작업을 진행할 때 필수적인 도구 중 하나예요. 퍼소나는 우리의 제품이나 서비스를 사용할 가상의 사용자를 대표하는 캐릭터로, 실제 사용자 데이터를 기반으로 만들어집니다. 이를 통해 디자이너는 사용자 중심의 디자인을 구현할 수 있어요. 퍼소나를 만드는 과정은 다음과 같습니다. 먼저, 사용자 리서치를 통해 목표 사용자의 행동, 동기, 요구사항 등을 파악해요. 이때 인터뷰, 설문조사, 사용성 테스트 등의 방법을 사용합니다. 수집된 데이터를 분석하여 공통된 특성과 패턴을 찾아내고, 이를 바탕으로 하나 이상의 퍼소나를 정의해요.퍼소나는 예를 들어, 우리의 목표 사용자가 30대 직장인이라면, 그들의 하루 일과, 주요 관심사, 직장에서 겪는 문제점 등을 자세히 기록하는 편이에요. 퍼소나는 디자인 과정에서 여러 가지 중요한 역할을 합니다. 첫째, 팀원들이 사용자에 대한 공통된 이해를 갖게 해요. 이는 의사소통을 원활하게 하고, 팀원들이 같은 방향을 바라보도록 도와줍니다. 둘째, 디자인 결정 시 사용자 관점을 유지해 사용자에게 실제로 필요한 기능과 경험을 제공할 수 있습니다. 셋째, 사용자의 요구와 목표를 구체적으로 함으로써, 디자이너가 더 창의적이고 효율적으로 문제를 해결할 수 있도록 해줘요. 예를 들어, 우리는 '김지훈'이라는 퍼소나를 만들 수 있습니다. 지훈은 35세의 마케팅 매니저로, 바쁜 업무 일정 속에서 효율적으";
@@ -15,26 +16,39 @@ const testTxt2 =
   "반갑습니다!!!, 여러분. 오늘은 디자인 프로세스에서 중요한 역할을 하는 퍼소나(Persona)에 대해 이야기해보겠습니다. 퍼소나는 디자인 작업을 진행할 때 필수적인 도구 중 하나예요. 퍼소나는 우리의 제품이나 서비스를 사용할 가상의 사용자를 대표하는 캐릭터로, 실제 사용자 데이터를 기반으로 만들어집니다. 이를 통해 디자이너는 사용자 중심의 디자인을 구현할 수 있어요. 퍼소나를 만드는 과정은 다음과 같습니다. 먼저, 사용자 리서치를 통해 목표 사용자의 행동, 동기, 요구사항 등을 파악해요. 이때 인터뷰, 설문조사, 사용성 테스트 등의 방법을 사용합니다. 수집된 데이터를 분석하여 공통된 특성과 패턴을 찾아내고, 이를 바탕으로 하나 이상의 퍼소나를 만듭니다.퍼소나는 예를 들어, 우리의 목표 사용자가 30대 직장인이라면, 그들의 하루 일과, 주요 관심사, 직장에서 겪는 문제점 등을 자세히 기록하는 편이에요. 퍼소나는 디자인 과정에서 여러 가지 중요한 역할을 합니다. 첫째, 팀원들이 사용자에 대한 공통된 이해를 갖게 해요. 이는 의사소통을 원활하게 하고, 팀원들이 같은 방향을 바라보도록 도와줍니다. 둘째, 디자인 결정 시 사용자 관점을 유지해 사용자에게제로 필요한 기능과 경험을 제공할 수 있습니다. 셋째, 사용자의 요구와 목표를 구체적으로 함으로써, 디자이너가 더 창의적이고 효율적으로 문제를 해결할 수 있도록 해줘요. 예를 들어, 우리는 '김지훈'이라는 퍼소나를 만들 수 있습니다. 지훈은 35세의 마케팅 매니저로, 바쁜 업무 일정 속에서 효율적으로 관리한니다";
 
 export default function Announce() {
-  const [originScript, setOriginScript] = useState('');
-  const [newScript, setNewScript] = useState('');
+  const {
+    originScript,
+    setOriginScript,
+    newScript,
+    setNewScript,
+    subject,
+    setSubject,
+    presentPurpose,
+    setPresentPurpose,
+    endingTxt,
+    setEndingTxt,
+    highlightedText,
+    setHighlightedText,
+    askListState,
+    setAskListState,
+    askListTotalShow,
+    setAskListTotalShow,
+    showSkeleton,
+    setShowSkeleton,
+    showNewSkeleton,
+    setShowNewSkeleton,
+  } = useStore();
+
   const [charCount, setCharCount] = useState(0);
   const [charCountNew, setCharCountNew] = useState(0);
-  const [subject, setSubject] = useState('');
   const [subjectCharCount, setSubjectCharCount] = useState(0);
-  const [presentPurpose, setpresentPurpose] = useState('회사 컨퍼런스');
-  const [endingTxt, setEndingTxt] = useState('합니다체');
   const [modifyBtn, setModifyBtn] = useState(false);
   const [estimatedPresentTime, setEstimatedPresentTime] = useState('0분 0초'); // 예상 발표 시간
   const [repeat, setRepeat] = useState(false);
-  const [askListState, setAskListState] = useState([false, false, false]);
-  const [askListTotalShow, setAskLisTotalShow] = useState(false);
 
   // 교정본 수정 여부 확인용 state
   const [retryScriptCompareTxt, setRetryScriptCompareTxt] = useState('');
-
   const [scriptToggle, setScriptToggle] = useState(false);
-  const [highlightedText, setHighlightedText] = useState([]);
-
   const scriptWriteBoxRef = useRef(null);
   const [containerWidth, setContainerWidth] = useState(725);
 
@@ -43,12 +57,10 @@ export default function Announce() {
   const scriptUpdateTextareaRef = useRef(null);
   const skeletonRef = useRef(null);
   const skeletonNewScriptRef = useRef(null);
-  const [showSkeleton, setShowSkeleton] = useState(false);
-  const [showNewSkeleton, setShowNewSkeleton] = useState(false);
   const [lineWidths, setLineWidths] = useState([]);
 
   const selectPurpose = (purpose) => {
-    setpresentPurpose(purpose);
+    setPresentPurpose(purpose);
   };
 
   const selectEndingTxt = (txt) => {
@@ -93,7 +105,7 @@ export default function Announce() {
     setSubject('');
     setCharCount(0);
     setSubjectCharCount(0);
-    setpresentPurpose('회사 컨퍼런스');
+    setPresentPurpose('회사 컨퍼런스');
     setEndingTxt('합니다체');
     setRepeat(false);
     setEstimatedPresentTime('0분 0초');
@@ -117,6 +129,7 @@ export default function Announce() {
   // 질문 펼침 초기화
   useEffect(() => {
     setAskListState([false, false, false]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const highlightDiffs = (oldStr, newStr) => {
@@ -180,7 +193,7 @@ export default function Announce() {
           highlightDiffs(oldScript, updatedScript);
 
           // 나머지 상태 업데이트
-          setAskLisTotalShow(true);
+          setAskListTotalShow(true);
           setScriptToggle(true);
           setAskListState([false, false, false]);
         } else {
@@ -200,7 +213,7 @@ export default function Announce() {
           }
 
           // 나머지 상태 업데이트
-          setAskLisTotalShow(true);
+          setAskListTotalShow(true);
           setScriptToggle(true);
           setAskListState([false, false, false]);
         }
---
+++ src/pages/store/store.js
@@ -0,0 +1,26 @@
+import create from 'zustand';
+
+const useStore = create((set) => ({
+  originScript: '',
+  setOriginScript: (script) => set({ originScript: script }),
+  newScript: '',
+  setNewScript: (script) => set({ newScript: script }),
+  subject: '',
+  setSubject: (subject) => set({ subject }),
+  presentPurpose: '회사 컨퍼런스',
+  setPresentPurpose: (purpose) => set({ presentPurpose: purpose }),
+  endingTxt: '합니다체',
+  setEndingTxt: (txt) => set({ endingTxt: txt }),
+  highlightedText: [],
+  setHighlightedText: (texts) => set({ highlightedText: texts }),
+  askListState: [false, false, false],
+  setAskListState: (state) => set({ askListState: state }),
+  askListTotalShow: false,
+  setAskListTotalShow: (show) => set({ askListTotalShow: show }),
+  showSkeleton: false,
+  setShowSkeleton: (show) => set({ showSkeleton: show }),
+  showNewSkeleton: false,
+  setShowNewSkeleton: (show) => set({ showNewSkeleton: show }),
+}));
+
+export default useStore;
