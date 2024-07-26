434341d42e2acb2f6f35f54a549e6f0274fd3c3e
minsun309 <pminsun309@gmail.com>
2024-07-27 00:26:16 +0900

    Feat : 게이트웨이 페이지 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,10 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import '@/styles/main.css';
+import '@/styles/gateway.css';
 import '@/styles/fonts/notoSansKR.css';
+import '@/styles/fonts/tmoneyRoundWind.css';
+import '@/styles/fonts/appleSDGothicNeoB.css';
 
 export default function App({ Component, pageProps }) {
   return (
--- src/utils/imageImports.js
+++ src/utils/imageImports.js
@@ -1,5 +1,8 @@
 import ImageLogo from '../../public/images/Logo.svg';
 import ImageMobileLogo from '../../public/images/mobile_logo.svg';
+import ImageVarLogo from '../../public/images/Logo_var.svg';
+import ImageBenner from '../../public/images/Benner.svg';
+
 import ImageTtorangArch from '../../public/images/ttorang_arch.svg';
 import ImageTtorangNote from '../../public/images/ttorang_note.svg';
 
@@ -10,6 +13,22 @@ import ImageIconDescription from '../../public/images/icon_description.svg';
 import ImageIconHelp from '../../public/images/icon_help.svg';
 import ImageIconSyncAlt from '../../public/images/icon_sync_alt.svg';
 
-export { ImageLogo, ImageMobileLogo, ImageTtorangArch, ImageTtorangNote };
+import ImageGatewayBenner from '../../public/images/gatewayBanner.svg';
+import ImageGatewayDetailInfo from '../../public/images/gate_detailInfo.svg';
+import ImageGatewayAskBG from '../../public/images/gateway_askBg.svg';
+import ImageGatewayAskPoint from '../../public/images/gateway_askPoint.svg';
+import ImageGatewayFeedback from '../../public/images/gateway_feedback.svg';
+import ImageGatewayModify from '../../public/images/gateway_modify.svg';
+
+import ImageGatewayOctagon from '../../public/images/gatway_octagon.svg';
+import ImageGatewayRound from '../../public/images/gatway_round.svg';
+import ImageGatewayFlower from '../../public/images/gatway_flower.svg';
+import ImageGatewayHexagon from '../../public/images/gateway_hexagon.svg';
+
+export { ImageLogo, ImageMobileLogo, ImageVarLogo, ImageBenner, ImageTtorangArch, ImageTtorangNote };
 
 export { ImageIconCheckboxArrow, ImageIconArrow, ImageIconCopy, ImageIconDescription, ImageIconHelp, ImageIconSyncAlt };
+
+export { ImageGatewayBenner, ImageGatewayDetailInfo, ImageGatewayAskBG, ImageGatewayAskPoint, ImageGatewayFeedback, ImageGatewayModify };
+
+export { ImageGatewayOctagon, ImageGatewayRound, ImageGatewayFlower, ImageGatewayHexagon };
