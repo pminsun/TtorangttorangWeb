f447e7f1bcf1887b48a324a2fe8fd7321a4fe6b0
pminsun <pminsun309@gmail.com>
2024-07-27 09:09:12 +0000

    Fix: Mixed Content 에러

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // 모든 경로에 대해 Content-Security-Policy 헤더를 설정합니다.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
