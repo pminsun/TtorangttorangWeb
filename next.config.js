2654aca2a831775ef3560204f60e928c3529ded1
minsun309 <pminsun309@gmail.com>
2024-07-27 18:40:39 +0900

    Test : rewrites 제거

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/clova/script', // 요청한 경로
-        destination: `https://ttorang.site:8080/api/clova/scrip`, // 내가 사용할 경로
-      },
-    ];
-  },
 };
