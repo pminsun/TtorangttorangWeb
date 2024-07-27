abfb6df0e6bc41f56ad98c6be51bcd8e31b621f3
minsun309 <pminsun309@gmail.com>
2024-07-27 18:38:52 +0900

    Test : rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/clova/script', // 요청한 경로
+        destination: `https://ttorang.site:8080/api/clova/scrip`, // 내가 사용할 경로
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
