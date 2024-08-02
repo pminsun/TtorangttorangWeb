58aee490f7392618333b937446e30c0f34d6ab4a
minsun309 <pminsun309@gmail.com>
2024-08-02 23:45:31 +0900

    Feat : header 카카오 로그인 연동 maxAge 1시간 변경

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,4 +8,27 @@ export default NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET || '',
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      // 세션 만료 시간 설정
+      session.expires = token.exp * 1000;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        // JWT 만료 시간을 현재 시간 + 1시간으로 설정
+        //token.exp = Math.floor(Date.now() / 1000) + 60 * 60;
+        token.exp = Math.floor(Date.now() / 1000) + 60;
+      }
+      return token;
+    },
+  },
+  session: {
+    // 세션 전략을 JWT로 설정
+    strategy: 'jwt',
+    //maxAge: 60 * 60, // 1시간
+    maxAge: 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
