75d006d35890561fab2d76b4a60b26f701ac3e95
pminsun <pminsun309@gmail.com>
2024-08-02 14:17:17 +0000

    Feat : header 카카오 로그인 연동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,45 @@
 import Image from 'next/image';
 import * as LocalImages from '@/utils/imageImports';
 import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Header() {
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <header>
-      <Link
-        href={'/'}
-        className="logo_area"
-      >
-        <Image
-          src={LocalImages.ImageLogo}
-          alt="ImageLogo"
-          width={120}
-          height={52}
-        />
-      </Link>
+      <div className="header_container">
+        <div className="header_menu">
+          <h1>
+            <Link
+              href={'/'}
+              className="logo_area"
+            >
+              <Image
+                src={LocalImages.ImageVarLogo}
+                alt="ImageVarLogo"
+                width={118}
+                height={53}
+              />
+            </Link>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <Link href={'/'}>메인 홈</Link>
+              </li>
+              <li>
+                <Link href={'/'}>교정하기</Link>
+              </li>
+              <li>
+                <button onClick={() => signOut('kakao')}>로그아웃</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="header_user">{session?.user.name ? <Link href={'/'}>마이페이지</Link> : <button onClick={() => signIn('kakao')}>로그인</button>}</div>
+      </div>
     </header>
   );
 }
