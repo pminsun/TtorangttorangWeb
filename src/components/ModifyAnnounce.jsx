80593d543d7ecd812bf663ecb3cc0236b1ee49c9
minsun309 <pminsun309@gmail.com>
2024-08-03 23:44:02 +0900

    Feat : 예상질문 페이지 완성 발표문

--- src/components/ModifyAnnounce.jsx
+++ src/components/ModifyAnnounce.jsx
@@ -5,11 +5,21 @@ import * as LocalImages from '@/utils/imageImports';
 import { cls, formatNumber, measureTextWidth } from '@/utils/config';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { fetchAnnounceData } from '@/api/fetchData';
-import SkeletonLoading from './SkeletonLoading';
 import HighlightWithinTextarea from 'react-highlight-within-textarea';
 import { diffWords } from 'diff';
+import { useNextMoveBtnStore, useSettingStore, useFinalScriptStore } from '@/store/store';
 
 export default function ModifyAnnounce() {
+  const { nextMoveBtn, setNextMoveBtn } = useNextMoveBtnStore();
+  const { finalScript, setFinalScript } = useFinalScriptStore();
+  // setting
+  const { presentPurpose, setPresentPurpose, endingTxt, setEndingTxt, repeat, setRepeat } = useSettingStore();
+  useEffect(() => {
+    setPresentPurpose('회사 컨퍼런스');
+    setEndingTxt('합니다체');
+    setRepeat(false);
+  }, []);
+
   // 초안
   const [originScript, setOriginScript] = useState('');
   const [charCountOrigin, setCharCountOrigin] = useState(0);
@@ -25,23 +35,10 @@ export default function ModifyAnnounce() {
   const [compareScriptToggle, setcompareScriptToggle] = useState(false);
   const [highlightedText, setHighlightedText] = useState([]);
   const scriptWriteBoxRef = useRef(null);
-  // setting
-  const [presentPurpose, setPresentPurpose] = useState('회사 컨퍼런스');
-  const [endingTxt, setEndingTxt] = useState('합니다체');
-  const [repeat, setRepeat] = useState(false);
   // 예상 발표 시간
   const [estimatedPresentTime, setEstimatedPresentTime] = useState('0분 0초');
-  //skeleton
-  const [containerWidth, setContainerWidth] = useState(550);
-  const scriptTextareaRef = useRef(null);
-  const scriptUpdateTextareaRef = useRef(null);
-  const skeletonRef = useRef(null);
-  const skeletonNewScriptRef = useRef(null);
-  const [showSkeleton, setShowSkeleton] = useState(false);
-  const [showNewSkeleton, setShowNewSkeleton] = useState(false);
-  const [lineWidths, setLineWidths] = useState([]);
-
-  console.log(lineWidths);
+  // 로딩
+  const [loading, setLoading] = useState(false);
 
   // 초안 작성
   const writeOriginScript = (event) => {
@@ -89,6 +86,7 @@ export default function ModifyAnnounce() {
   // script 초기화 버튼
   const deleteAllScript = () => {
     setOriginScript('');
+    setNewScript('');
     setSubject('');
     setCharCountOrigin(0);
     setSubjectCharCount(0);
@@ -108,7 +106,7 @@ export default function ModifyAnnounce() {
     }
   }, [originScript, subject, newScript, compareScriptToggle]);
 
-  const filterOut = ['-', '"', '"', '!.', '!', '['];
+  const filterOut = ['-', '"', '"', '!.', '!', '[', ']', ':'];
   const highlightDiffs = (oldStr, newStr) => {
     const diff = diffWords(oldStr, newStr);
     const highlights = [];
@@ -129,17 +127,7 @@ export default function ModifyAnnounce() {
 
   //  교정하기 버튼
   const modifyScript = async () => {
-    // Skeleton loading start
-    if (scriptTextareaRef.current) {
-      setShowSkeleton(true);
-      scriptTextareaRef.current.style.opacity = '0';
-    }
-
-    if (newScript.length > 0 && modifyBtn && compareScriptToggle && scriptUpdateTextareaRef.current) {
-      setShowNewSkeleton(true);
-      scriptUpdateTextareaRef.current.style.opacity = '0';
-    }
-
+    setLoading(true);
     try {
       const data = {
         topic: subject,
@@ -150,97 +138,56 @@ export default function ModifyAnnounce() {
       };
 
       const response = await fetchAnnounceData(data);
-      let jsonStringArray = [];
-      let dataArray = [];
-      jsonStringArray.push(response.data);
+      const redData = response.data.replace(/data:/g, '');
+      const events = redData.split('\n\n'); // 이벤트 분리
+      const newContentQueue = [];
+      events.forEach((event) => {
+        if (event.trim()) {
+          try {
+            const jsonData = JSON.parse(event);
+            const content = jsonData.message?.content || '';
 
-      jsonStringArray.forEach((jsonString) => {
-        // 문자열을 줄 단위로 분리
-        const lines = jsonString.split('\n');
-
-        // 각 줄을 처리하여 JSON 객체로 변환
-        lines.forEach((line) => {
-          // 'data:' 접두사 제거
-          const jsonString = line.replace(/^data:/, '').trim();
-
-          // JSON 문자열이 비어 있지 않은 경우
-          if (jsonString) {
-            try {
-              const parsedObject = JSON.parse(jsonString);
-
-              // 'message'와 'content'가 있는 경우에만 추출
-              if (parsedObject.message && parsedObject.message.content) {
-                dataArray.push(parsedObject.message.content);
-              }
-            } catch (e) {
-              console.error('Error parsing JSON string:', e);
+            if (content) {
+              // 상태를 업데이트하여 새 content 값을 배열에 추가
+              newContentQueue.push(content);
             }
+          } catch (error) {
+            console.error('Failed to parse JSON:', error);
           }
-        });
+        }
       });
 
-      const finaldata = dataArray.join('');
-      highlightDiffs(originScript, finaldata);
-      //setNewScript((prevQueue) => [...prevQueue, ...newContentQueue]);
-      setNewScript(finaldata);
+      const finaldata = newContentQueue.join('');
 
-      // 첫 번째 교정
+      console.log(finaldata);
+
+      // 재교정 시 (2회차 이상)
+      if (newScript.length > 0 && modifyBtn && compareScriptToggle) {
+        const oldScript = newScript.slice(0, 3000);
+        const updatedScript = finaldata;
+        console.log('Old Script:', oldScript); // 로그 추가
+        console.log('Updated Script:', updatedScript); // 로그 추가
+        // 2회차 새로운 교정본을 newScript로 설정 1회차는 구
+        setOriginScript(oldScript);
+        setNewScript(updatedScript);
+        setFinalScript(updatedScript);
+        highlightDiffs(oldScript, updatedScript);
+      } else {
+        // 첫 번째 교정
+        highlightDiffs(originScript, finaldata);
+        setNewScript(finaldata);
+        setFinalScript(finaldata);
+      }
+
+      setCharCountNew(finaldata.length);
       setcompareScriptToggle(true);
-      setShowSkeleton(false);
-
-      if (scriptUpdateTextareaRef.current) {
-        scriptUpdateTextareaRef.current.style.opacity = '1';
-      }
-      if (scriptTextareaRef.current) {
-        scriptTextareaRef.current.style.opacity = '1';
-      }
+      setLoading(false);
+      setNextMoveBtn(true);
     } catch (error) {
       console.error('Error fetching modified script:', error);
-      setShowSkeleton(false);
+      setLoading(false);
     }
   };
-
-  // 스켈레톤 로딩
-  useEffect(() => {
-    const lines = compareScriptToggle ? newScript.split('\n') : originScript.split('\n');
-    const skeletonLineWidths = lines.map((line) => measureTextWidth(line, '16px NotoSansKR'));
-    setLineWidths(skeletonLineWidths);
-  }, [newScript, originScript, compareScriptToggle]);
-
-  // script box width 동적
-  useEffect(() => {
-    const updateWidth = () => {
-      if (scriptWriteBoxRef.current) {
-        setContainerWidth(scriptWriteBoxRef.current.offsetWidth);
-      }
-    };
-
-    // Update width on mount and on resize
-    updateWidth();
-    window.addEventListener('resize', updateWidth);
-
-    return () => {
-      window.removeEventListener('resize', updateWidth);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (skeletonRef.current) {
-        skeletonRef.current.style.top = `-${scriptTextareaRef.current.scrollTop}px`;
-      }
-    };
-
-    const textarea = scriptTextareaRef.current;
-    if (textarea) {
-      textarea.addEventListener('scroll', handleScroll);
-    }
-    return () => {
-      if (textarea) {
-        textarea.removeEventListener('scroll', handleScroll);
-      }
-    };
-  }, []);
 
   return (
     <section className="main_container">
@@ -252,7 +199,7 @@ export default function ModifyAnnounce() {
               firstMent={'발표문 초안 작성'}
               secondMent={'또랑또랑의 세심한 교정으로 더욱 명확하고 논리적인 발표문을 만들어 드려요'}
             />
-            <div ref={scriptWriteBoxRef}>
+            <div>
               <div className="scriptMain_area">
                 <p className="title">
                   <span className="required">*</span>발표 내용
@@ -260,64 +207,43 @@ export default function ModifyAnnounce() {
                 <div className="scriptTxt">
                   {!compareScriptToggle && (
                     <>
-                      <textarea
-                        ref={scriptTextareaRef}
-                        placeholder="발표문 초안을 작성해 주세요. 꼼꼼히 작성할수록 세심한 교정과 정확한 예상 질문을 받을 수 있어요."
-                        maxLength="3000"
-                        value={originScript}
-                        onChange={writeOriginScript}
-                      />
-                      <p>{formatNumber(charCountOrigin)} / 3000</p>
                       <div
-                        ref={skeletonRef}
-                        className="skeleton_box"
-                        style={{ pointerEvents: 'none' }}
+                        ref={scriptWriteBoxRef}
+                        className="textSize"
                       >
-                        {showSkeleton && (
-                          <SkeletonLoading
-                            lineWidths={lineWidths}
-                            containerWidth={containerWidth}
-                          />
-                        )}
+                        <textarea
+                          placeholder="발표문 초안을 작성해 주세요. 꼼꼼히 작성할수록 세심한 교정과 정확한 예상 질문을 받을 수 있어요."
+                          maxLength="3000"
+                          value={originScript}
+                          onChange={writeOriginScript}
+                        />
                       </div>
+                      <p>{formatNumber(charCountOrigin)} / 3000</p>
                     </>
                   )}
                   {compareScriptToggle && (
                     <>
-                      <div
-                        className="newScript"
-                        ref={scriptUpdateTextareaRef}
-                      >
-                        <HighlightWithinTextarea
-                          highlight={[
-                            {
-                              highlight: highlightedText.filter((item) => !filterOut.includes(item)),
-                              className: 'bg-[#cbeaff]',
-                            },
-                          ]}
-                          value={newScript}
-                          onChange={(value) => {
-                            setNewScript(value);
-                            setCharCountNew(value.length);
-                            if (value.length > 3000) {
-                              setCharCountNew(3000);
-                            }
-                          }}
-                        />
+                      <div className="textSize">
+                        <div className="newScript">
+                          <HighlightWithinTextarea
+                            highlight={[
+                              {
+                                highlight: highlightedText.filter((item) => !filterOut.includes(item) && item.length > 1),
+                                className: '!bg-[#cbeaff]',
+                              },
+                            ]}
+                            value={newScript}
+                            onChange={(value) => {
+                              setNewScript(value);
+                              setCharCountNew(value.length);
+                              if (value.length > 3000) {
+                                setCharCountNew(3000);
+                              }
+                            }}
+                          />
+                        </div>
                       </div>
                       <p className="mt-[5px]">{formatNumber(charCountNew)} / 3000</p>
-                      <div
-                        ref={skeletonNewScriptRef}
-                        className="skeleton_box"
-                        style={{ pointerEvents: 'none' }}
-                      >
-                        {showNewSkeleton && (
-                          <SkeletonLoading
-                            lineWidths={lineWidths}
-                            containerWidth={containerWidth}
-                          />
-                        )}
-                      </div>
                     </>
                   )}
                 </div>
--- src/pages/announce/index.js
+++ src/pages/announce/index.js
@@ -1,75 +1,87 @@
-import GuideMent from '@/components/GuideMent';
-import SkeletonLoading from '@/components/SkeletonLoading';
-import { cls, formatNumber, measureTextWidth, temData, dataT } from '@/utils/config';
-import { diffWords } from 'diff';
 import { useEffect, useState, useRef } from 'react';
-import HighlightWithinTextarea from 'react-highlight-within-textarea';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Image from 'next/image';
 import * as LocalImages from '@/utils/imageImports';
-import { fetchAnnounceData } from '@/api/fetchData';
 
 import Slider from 'react-slick';
 import ModifyAnnounce from '@/components/ModifyAnnounce';
 import SaveAnnounce from '@/components/SaveAnnounce';
 import ProgressBar from '@/components/ProgressBar';
-
-function NextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', cursor: 'pointer', zIndex: 50 }}
-      onClick={onClick}
-    >
-      <Image
-        src={LocalImages.ImageMainStepArrowRight}
-        alt="ImageMainStepArrowRight"
-        width={80}
-        height={80}
-      />
-    </div>
-  );
-}
-
-function PrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', cursor: 'pointer', zIndex: 50 }}
-      onClick={onClick}
-    >
-      <Image
-        src={LocalImages.ImageMainStepArrowLeft}
-        alt="ImageMainStepArrowLeft"
-        width={80}
-        height={80}
-      />
-    </div>
-  );
-}
-
-const settings = {
-  dots: false,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  nextArrow: <NextArrow />,
-  prevArrow: <PrevArrow />,
-};
+import { useNextMoveBtnStore } from '@/store/store';
 
 export default function Announce() {
+  const { nextMoveBtn } = useNextMoveBtnStore();
   const [currentSlide, setCurrentSlide] = useState(0);
+
+  function NextArrow(props) {
+    const { nextMoveBtn } = useNextMoveBtnStore();
+    const { className, style, onClick } = props;
+
+    const dynamicStyle = {
+      ...style,
+      display: nextMoveBtn ? 'block' : 'none',
+      cursor: 'pointer',
+      zIndex: 50,
+    };
+
+    return (
+      <div
+        className={className}
+        style={dynamicStyle}
+        onClick={onClick}
+      >
+        <Image
+          src={LocalImages.ImageMainStepArrowRight}
+          alt="ImageMainStepArrowRight"
+          width={80}
+          height={80}
+        />
+      </div>
+    );
+  }
+
+  function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: 'block', cursor: 'pointer', zIndex: 50 }}
+        onClick={onClick}
+      >
+        <Image
+          src={LocalImages.ImageMainStepArrowLeft}
+          alt="ImageMainStepArrowLeft"
+          width={80}
+          height={80}
+        />
+      </div>
+    );
+  }
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    draggable: false,
+    swipe: false,
+    beforeChange: (current, next) => {
+      if (!nextMoveBtn) {
+        return false; // nextMoveBtn이 false일 경우 슬라이드 변경을 막음
+      }
+    },
+    afterChange: (next) => {
+      // 슬라이드가 변경된 후 상태 업데이트
+      setCurrentSlide(next);
+    },
+  };
 
   return (
     <div className="slider-container">
       <ProgressBar currentSlide={currentSlide} />
-      <Slider
-        {...settings}
-        beforeChange={(currentSlide, nextSlide) => setCurrentSlide(nextSlide)}
-      >
+      <Slider {...settings}>
         <ModifyAnnounce />
         <SaveAnnounce />
       </Slider>
