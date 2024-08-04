a9890b5418a3d8226a5814d9673811c78096f985
minsun309 <pminsun309@gmail.com>
2024-08-04 23:24:02 +0900

    Feat : 선 작성 후 로그인 시 작성문 유지 - session 다른페이지 이동시 초기값으로 변경

--- src/components/ModifyAnnounce.jsx
+++ src/components/ModifyAnnounce.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import GuideMent from './GuideMent';
 import * as LocalImages from '@/utils/imageImports';
 import { cls, formatNumber } from '@/utils/config';
@@ -10,6 +11,7 @@ import { diffWords } from 'diff';
 import { useNextMoveBtnStore, useSettingStore, useInitialSettingStore, useFinalScriptStore, useScriptLoadingStore } from '@/store/store';
 
 export default function ModifyAnnounce({ session }) {
+  const pathname = usePathname();
   const { setNextMoveBtn } = useNextMoveBtnStore();
   const { setFinalScript } = useFinalScriptStore();
   const { setScriptLoading } = useScriptLoadingStore();
@@ -47,6 +49,20 @@ export default function ModifyAnnounce({ session }) {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session]);
+
+  useEffect(() => {
+    // 경로가 변경될 때 상태 초기화
+    if (session && pathname !== '/announce') {
+      setOriginScript('');
+      setSubject('');
+      setPresentPurpose('회사 컨퍼런스');
+      setEndingTxt('합니다체');
+      setRepeat(false);
+      setNewScript('');
+      setNextMoveBtn(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname, session]);
 
   // 초안
   const [charCountOrigin, setCharCountOrigin] = useState(0);
