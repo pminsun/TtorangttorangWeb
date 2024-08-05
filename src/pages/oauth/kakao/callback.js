a6bd57ddc5a2229418b2330a95c3822fa787df9a
minsun309 <pminsun309@gmail.com>
2024-08-05 22:59:10 +0900

    Feat: 카카오 access token 받기

--- src/pages/oauth/kakao/callback.js
+++ src/pages/oauth/kakao/callback.js
@@ -22,12 +22,36 @@ const KakaoCallback = () => {
     // 카카오 인증 코드가 있는 경우 처리
     if (code) {
       // 백엔드 서버에 GET 요청을 보냅니다
-      fetch(`https://api.ttorang.site/oauth/kako/callback?code=${code}`)
+      // fetch(`https://api.ttorang.site/oauth/kakao/callback?code=${code}`)
+      //   .then((response) => {
+      //     response.json();
+      //     console.log(response);
+      //   })
+      //   .catch(() => {});
+
+      const grant_type = 'authorization_code&';
+      const client_id = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+      const client_secret = process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET;
+      const redirect_uri = 'https://api.ttorang.site/oauth/kakao/callback';
+
+      const url = `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=http://localhost:3000/oauth/kakao/callback&code=${code}`;
+
+      axios
+        .post(
+          url,
+          {},
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+            },
+          },
+        )
         .then((response) => {
-          response.json();
-          console.log(response);
+          console.log('Response:', response.data);
         })
-        .catch(() => {});
+        .catch((error) => {
+          console.error('Error:', error.response ? error.response.data : error.message);
+        });
     }
   }, [router.query.code, router]);
 
