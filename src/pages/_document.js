be183c7c3687639bc1920f2cda8d957e3269ba73
minsun309 <pminsun309@gmail.com>
2024-08-11 22:52:39 +0900

    Feat: title 명 변경

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
+        <title>또랑또랑</title>
+        <meta
+          name="description"
+          content="또랑또랑 AI 발표 도우미"
+        />
         <link
           rel="icon"
           href="/images/favicon.ico"
